96f8269ac17efbd3b9f9b5da80ef12be273e6e98
lunar <2100125584@qq.com>
2024-09-10 22:01:30 +0800

    basic test

--- labrpc/src/lib.rs
+++ labrpc/src/lib.rs
@@ -42,25 +42,76 @@ pub(crate) struct Msg {
     pub reply_tx: OneTx<CallResult>
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use std::time::Duration;
-//
-//     use labrpc_macros::rpc;
-//     use crate::{err::{self, DISCONNECTED, TIMEOUT}, CallResult, Service};
-//
-//     struct Hello;
-//
-//     #[rpc(Service, CallResult, err)]
-//     impl Hello {
-//         pub fn hello(&self, name: String) -> String {
-//             format!("Hello, {name}")
-//         }
-//
-//         pub async fn no_response(&self, _: ()) {
-//             // sleep a long time to simulate the situation where
-//             // the service is unresponsive.
-//             tokio::time::sleep(Duration::from_secs(120)).await;
-//         }
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use labrpc_macros::rpc;
+    use crate::{err::{self, CLASS_NOT_FOUND}, network::Network, CallResult, Service};
+
+    struct Hello;
+    #[rpc(Service, CallResult, err)]
+    impl Hello {
+        pub fn hello(&self, name: String) -> String {
+            format!("Hello, {name}")
+        }
+    }
+
+    #[tokio::test]
+    async fn basic() {
+        const N: usize = 3;
+        let mut network = Network::new(N);
+        let mut clients = Vec::with_capacity(N);
+        for i in 0..N {
+            let cli = network.make_client(i).await;
+            cli.add_service(
+                "Hello".to_string(),
+                Box::new(Hello)
+            ).await;
+            clients.push(cli);
+        }
+
+        {
+            let c0 = &clients[0];
+            let mut cnt = 0;
+            for peer in c0.peers() {
+                assert_eq!(
+                    Ok("Hello, Lunar".to_string()),
+                    peer.call::<_, String>("Hello.hello", "Lunar".to_string()).await
+                );
+                cnt += 1;
+            }
+            assert_eq!(2, cnt);
+        }
+
+        // make a new client at index 0, add a same service with 
+        // different name, expect the original server is removed.
+        {
+            let c0 = &mut clients[0];
+            *c0 = network.make_client(0).await;
+            c0.add_service("Hello2".to_string(), 
+                Box::new(Hello)).await;
+        }
+        
+        {
+            let c1 = &clients[1];
+            let mut res = Vec::new();
+            for peer in c1.peers() {
+                res.push(
+                    peer.call::<_, String>("Hello.hello", "Lunar".to_string()).await
+                );
+            }
+            assert_eq!(2, res.len());
+
+            let count = |expect| -> usize {
+                res.iter()
+                    .fold(0, |acc, r| {
+                        acc + if r == expect {
+                            1
+                        } else { 0 }
+                    })
+            };
+            let ok = Ok("Hello, Lunar".to_string());
+            assert_eq!(1, count(&ok));
+            assert_eq!(1, count(&Err(CLASS_NOT_FOUND)));
+        }
+    }
+}
